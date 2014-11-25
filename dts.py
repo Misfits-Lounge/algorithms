de00f95a00078e71ae91929a056eedecbc17bfaa
Victor-p <palade.ionut@gmail.com>
2014-11-25 08:38:13 +0100

    data strctures

---
+++ dts.py
@@ -0,0 +1,10 @@
+
+'''Unpacking variables
+'''
+
+#discarding values when unpacking, can use _ var for it
+
+s = 'Hello'
+data = ['ACME', 50, 91.1, (2012,12,21)]
+_, shares, price, _ = data
+#print shares, price
