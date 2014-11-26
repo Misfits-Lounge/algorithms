fd9deada2b9f5ea3c5633c274f6d7dd072872183
Victor-p <palade.ionut@gmail.com>
2014-11-26 15:36:47 +0100

    classes

--- classes.py
+++ classes.py
@@ -3,8 +3,14 @@
 
 
 class BaseClass(object):
-    def __init__(self, name):
+    def __init__(self, name="class"):
         self.name = name
         
     def get_name(self):
         return self.name
+
+    def __str__(self):
+        return "class"
+
+
+print BaseClass()
